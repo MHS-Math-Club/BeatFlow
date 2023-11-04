c729522a03519bf5289a337c985c63c20e5941d5
Greasytugboat6 <78366801+Greasytugboat6@users.noreply.github.com>
2023-11-04 11:12:17 -0500

    added code to calculate cadence

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -16,13 +16,47 @@ function getCadence(){
     console.log("JS loaded")
     DeviceMotionEvent.requestPermission().then(response => {
         if (response == 'granted') {
+            let accelHist = []
+            let avgHist = []
+            let direction = true
+            let timeHist = []
             console.log("accelerometer permission granted");
             window.addEventListener('devicemotion', (event) => {
                 var headingElement = document.getElementById("accel_value");
                 var netAccel = Math.sqrt(event.acceleration.x ** 2 + event.acceleration.y ** 2 + event.acceleration.z ** 2)
                 let parsed = parseFloat(netAccel.toFixed(3));
+                if (accelHist.length() > 30) {
+                    accelHist.shift()
+                }
+                accelHist.push(netAccel)
+                let avgAccel = accelHist.reduce(adder) / accelHist.length
 
-                headingElement.textContent = netAccel.toString();
+                function adder(total, value, index, array){
+                    return total + value
+                }
+
+                if (avgHist.length() > 30) {
+                    avgHist.shift()
+                }
+                avgHist.push(avgAccel)
+                if(avgHist[0] < avgHist[avgHist.length - 1] != direction){
+                    direction =  !direction
+                }
+
+                if (timeHist.length() > 30){
+                    timeHist.shift()
+                }
+                timeHist.push(new Date().getTime())
+                let tdiffHist = [0]
+                for(i = 1; i < timeHist.length; i++){
+                    tdiffHist[i] = timeHist[i] - timeHist[i - 1]
+                }
+
+                avgDiff = tdiffHist.reduce(adder) / tdiffHist.length()
+
+                cadence = 60000 / avgDiff
+                
+                headingElement.textContent = cadence.toString();
             })
         }
         else{
