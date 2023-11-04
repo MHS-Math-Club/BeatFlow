58bb19fba97302dbe5ccbe7e3f7504ca2d5155e6
MarkusHoehn2007 <142701283+MarkusHoehn2007@users.noreply.github.com>
2023-11-04 16:33:51 -0500

    logging in

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import json
-from flask import Flask, render_template, request, redirect, session
-from spotify_requests import spotify
+import requests
+from flask import Flask, render_template, request, redirect, session, make_response, url_for
+from config import Config
+from functions import createStateKey, getToken, refreshToken, checkTokenStatus, getUserInformation
+import time
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
 app.secret_key = "something"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if 'auth_header' in session:
-        #
+    if 'user_id' in session:
+        # What to do when logged in aka play music
 
     playlist = []
     genre = "classical"
@@ -25,13 +30,35 @@ def index():
 
 @app.route("/auth")
 def auth():
-    return redirect(spotify.AUTH_URL)
+    client_id = app.config['CLIENT_ID']
+    client_secret = app.config['CLIENT_SECRET']
+    redirect_uri = app.config['REDIRECT_URI']
+    scope = app.config['SCOPE']
+
+    state_key = createStateKey(15)
+    session['state_key'] = state_key
+
+    authorize_url = 'https://accounts.spotify.com/en/authorize?'
+    parameters = 'response_type=code&client_id=' + client_id + '&redirect_uri=' + redirect_uri + '&scope=' + scope + '&state=' + state_key
+    response = make_response(redirect(authorize_url + parameters))
+
+    return response
 
 @app.route('/callback')
 def callback():
-    auth_token = request.args['code']
-    auth_header = spotify.authorize(auth_token)
-    session['auth_header'] = auth_header
+    code = request.args['code']
+    session.pop('state_key', None)
 
-    return index()
+    payload = getToken(code, app)
+
+    if payload != None:
+        session['token'] = payload[0]
+        session['refresh_token'] = payload[1]
+        session['token_expiration'] = time.time() + payload[2]
+
+    current_user = getUserInformation(session)
+    session['user_id'] = current_user['id']
+
+    return redirect(url_for('index')) 
+
 
---
+++ config.py
@@ -0,0 +1,6 @@
+class Config(object):
+	CLIENT_ID = "09d0ffca344548a5931c017a88cddda9"
+	CLIENT_SECRET = "ae66f254352447059ad0ecf5ffa268e8"
+	REDIRECT_URI = "http://127.0.0.1:5000/callback"
+	SCOPE = "user-modify-playback-state user-read-playback-state user-read-currently-playing"
+	AUTHORIZATION = ""
