ab55e015b220ddb4bd949923ada3d5d27453c47d
MarkusHoehn2007 <142701283+MarkusHoehn2007@users.noreply.github.com>
2023-11-04 08:40:02 -0500

    templating

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", song_name="Shake it Off", album_name="1989", artist_name="david", id="art")
