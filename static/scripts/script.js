54fce05a1bb960f53392cce740239a0d40510ecb
Greasytugboat6 <78366801+Greasytugboat6@users.noreply.github.com>
2023-11-04 13:42:36 -0500

    Readded cadence calculations

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -16,6 +16,10 @@ function getCadence() {
     console.log("JS loaded");
     DeviceMotionEvent.requestPermission().then(response => {
         if (response == 'granted') {
+            let accelHist = []
+            let avgHist = []
+            let direction = true
+            let timeHist = []
             console.log("accelerometer permission granted");
             var headingElementEnergy = document.getElementById("energy_value");
             var headingElement = document.getElementById("accel_value");
@@ -37,8 +41,38 @@ function getCadence() {
                     headingElementEnergy.textContent = "Energy: " + parsed90;
                 }
 
+                if (accelHist.length() > 30) {
+                    accelHist.shift()
+                }
+                accelHist.push(netAccel)
+                let avgAccel = accelHist.reduce(adder) / accelHist.length
+
+                function adder(total, value, index, array){
+                    return total + value
+                }
+
+                if (avgHist.length() > 30) {
+                    avgHist.shift()
+                }
+                avgHist.push(avgAccel)
+                if(avgHist[0] < avgHist[avgHist.length - 1] != direction){
+                    direction =  !direction
+                }
+
+                if (timeHist.length() > 30){
+                    timeHist.shift()
+                }
+                timeHist.push(new Date().getTime())
+                let tdiffHist = [0]
+                for(i = 1; i < timeHist.length; i++){
+                    tdiffHist[i] = timeHist[i] - timeHist[i - 1]
+                }
+
+                avgDiff = tdiffHist.reduce(adder) / tdiffHist.length()
+
+                cadence = 60000 / avgDiff
+
                 // Set another timeout for the next event processing after 100 milliseconds
-                setTimeout(processMotionEvent, 100);
             }
 
             // Add an event listener for the initial devicemotion event
