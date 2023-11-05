f438fa2ee54dd0322280d7e1b9f47de57d2781ee
Greasytugboat6 <78366801+Greasytugboat6@users.noreply.github.com>
2023-11-04 20:20:45 -0500

    fixed stats again

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -18,7 +18,6 @@ function getCadence() {
         if (response == 'granted') {
             let accelHist = []
             let avgHist = [0, 0, 0]
-            let direction = true
             let timeHist = []
             let longAccelHist = [1, 1]
             let cadence = 0
@@ -44,25 +43,30 @@ function getCadence() {
 
                     //peak detection
                     if((avgHist[0] + 0.3 < avgHist[Math.floor(avgHist.length / 2)] && avgHist[avgHist.length - 1] + 0.3 < avgHist[Math.floor(avgHist.length / 2)]) ){
-                        longAccelHist.push(netAccel)
-                        console.log('Peak detected at average acceleration: ' + avgHist[Math.floor(avgHist.length / 2)])
                         
-                        //log times
-                        if (timeHist.length > 5){
-                            timeHist.shift();
-                        }
-                        timeHist.push(new Date().getTime());
-                        let tdiffHist = [0]
-                        for(i = 1; i < timeHist.length; i++){
-                            tdiffHist[i] = timeHist[i] - timeHist[i - 1];
-                        }
+                        pTime = new Date().getTime();
+                        if(pTime - timeHist[timeHist.length] > 300){
 
-                        //calculate cadence
+                            longAccelHist.push(netAccel)
+                            console.log('Peak detected at average acceleration: ' + avgHist[Math.floor(avgHist.length / 2)])
+                            //log times
+                            if (timeHist.length > 20){
+                                timeHist.shift();
+                            }
+                        
+                            timeHist.push(pTime);
+                            let tdiffHist = [0];
+                            for(i = 1; i < timeHist.length; i++){
+                                tdiffHist[i] = timeHist[i] - timeHist[i - 1];
+                            }
+
+                            //calculate cadence
     
-                        avgDiff = tdiffHist.reduce(adder) / tdiffHist.length;
+                            avgDiff = tdiffHist.reduce(adder) / tdiffHist.length;
     
-                        cadence = 60000 / avgDiff;
-                        headingElementCadence.textContent = "Cadence: " + cadence;
+                            cadence = 60000 / avgDiff;
+                            headingElementCadence.textContent = "Cadence: " + cadence;
+                        }
                     }
                 }
                 
